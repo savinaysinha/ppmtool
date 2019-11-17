96b56b1d35b4a3320edcb86434bdc83fbd6da542
Savinay Sinha <Savinay Sinha>
2019-11-17 19:08:29 +0530

    project object added

--- ppmtoolbackend/PPMTOOL/src/main/java/com/savinetwork/ppmtool/Project.java
+++ ppmtoolbackend/PPMTOOL/src/main/java/com/savinetwork/ppmtool/Project.java
@@ -1,8 +1,110 @@
 package com.savinetwork.ppmtool;
 
-public class Project {
+import java.util.Date;
 
-	public void method() {
-		System.out.println("Hello");
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "projects")
+public class Project {
+	
+	public Project(){
+		
 	}
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String projectName;
+	private String projectIdentifier;
+	private String decription;
+	private Date startDate;
+	private Date endDate;
+	
+	private Date created_At;
+	private Date updated_At;
+	
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getProjectName() {
+		return projectName;
+	}
+
+	public void setProjectName(String projectName) {
+		this.projectName = projectName;
+	}
+
+	public String getProjectIdentifier() {
+		return projectIdentifier;
+	}
+
+	public void setProjectIdentifier(String projectIdentifier) {
+		this.projectIdentifier = projectIdentifier;
+	}
+
+	public String getDecription() {
+		return decription;
+	}
+
+	public void setDecription(String decription) {
+		this.decription = decription;
+	}
+
+	public Date getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+
+	public Date getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+
+	public Date getCreated_At() {
+		return created_At;
+	}
+
+	public void setCreated_At(Date created_At) {
+		this.created_At = created_At;
+	}
+
+	public Date getUpdated_At() {
+		return updated_At;
+	}
+
+	public void setUpdated_At(Date updated_At) {
+		this.updated_At = updated_At;
+	}
+
+	@PrePersist
+	protected void onCreate() {
+		this.created_At=new Date();
+	}
+	
+	@PreUpdate
+	protected void onUpdate() {
+		this.updated_At=new Date();
+	}
+	
+	
+
 }
