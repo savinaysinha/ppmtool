ee830c02f268a7928ceabb030943f599fa2f6995
Savinay Sinha <Savinay Sinha>
2019-11-17 19:28:27 +0530

    crud interface for project is created

--- ppmtoolbackend/PPMTOOL/src/main/java/com/savinetwork/ppmtool/repository/ProjectRepository.java
+++ ppmtoolbackend/PPMTOOL/src/main/java/com/savinetwork/ppmtool/repository/ProjectRepository.java
@@ -1,9 +1,11 @@
 package com.savinetwork.ppmtool.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.savinetwork.ppmtool.model.Project;
 
+@Repository
 public interface ProjectRepository extends CrudRepository<Project, Long>{
 	
 }
