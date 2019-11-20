dd512b3ab42357298209e0acd2fd04479da28fc7
Savinay Sinha <Savinay Sinha>
2019-11-21 04:36:02 +0530

    Valid Error Message output

--- ppmtoolbackend/PPMTOOL/src/main/java/com/savinetwork/ppmtool/web/ProjectController.java
+++ ppmtoolbackend/PPMTOOL/src/main/java/com/savinetwork/ppmtool/web/ProjectController.java
@@ -5,6 +5,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,7 +22,11 @@ public class ProjectController {
 	private ProjectService projectService;
 		
 	@PostMapping("")
-	public ResponseEntity<Project> saveOrUpdate(@Valid @RequestBody Project project){
+	public ResponseEntity<?> saveOrUpdate(@Valid @RequestBody Project project, BindingResult result){
+		if(result.hasErrors()) {
+			System.out.println(result.getFieldError());
+			return new ResponseEntity<String>("Project Object error",HttpStatus.BAD_REQUEST);
+		}
 		projectService.createProject(project);
 		return new ResponseEntity<Project>(project,HttpStatus.CREATED);
 	}
