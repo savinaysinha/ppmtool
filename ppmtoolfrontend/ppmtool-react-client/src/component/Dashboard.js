26cf0222a0e815597d42060565ca7eb8b9c2fa70
Savinay Sinha <Savinay Sinha>
2019-12-04 18:57:49 +0530

    put style to navbar,dashboard and project items with icons

--- ppmtoolfrontend/ppmtool-react-client/src/component/Dashboard.js
+++ ppmtoolfrontend/ppmtool-react-client/src/component/Dashboard.js
@@ -1,16 +1,37 @@
-import React, { Component } from 'react'
-import Project from './projectItems/Project'
-import Navbar from './layouts/Navbar'
+import React, { Component } from "react";
+import Navbar from "./layouts/Navbar";
+import ProjectItem from "./projectItems/ProjectItem";
 
- class Dashboard extends Component {
-    render() {
-        return(
-            <div>
-                <Navbar />
-                <h1 className="alert alert-warning">Welcome To Dashboard</h1>
-                <Project />
+class Dashboard extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <div className="projects">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-12">
+                <h1 className="display-4 text-center">Projects</h1>
+                <br />
+                <a href="ProjectForm.html" className="btn btn-lg btn-info">
+                  Create a Project
+                </a>
+                <br />
+                <hr />
+
+                {
+                  //<!-- Project Item Component -->
+                }
+                <ProjectItem />
+                {
+                  //<!-- End of Project Item Component -->
+                }
+              </div>
             </div>
-            )
-    }
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 export default Dashboard;
