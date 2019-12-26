0bf527f5af7fca4c53009fcf6bf46130c33538b5
Savinay Sinha <savinaysinha@gmail.com>
2019-12-26 07:56:27 +0530

    Added alert option while deleting project and proxy option in package.json file

--- ppmtoolfrontend/ppmtool-react-client/src/actions/ProjectActions.js
+++ ppmtoolfrontend/ppmtool-react-client/src/actions/ProjectActions.js
@@ -3,12 +3,12 @@ import { GET_ERRORS, GET_PROJECTS, GET_PROJECT, DELETE_PROJECT } from "./type";
 
 export const createProject = (project, history) => async dispatch => {
   try {
-    await axios.post("http://localhost:8080/api/project", project);
+    await axios.post("/api/project", project);
     history.push("/dashboard");
     dispatch({
-        type: GET_ERRORS,
-        payload: {}
-      });
+      type: GET_ERRORS,
+      payload: {}
+    });
   } catch (error) {
     dispatch({
       type: GET_ERRORS,
@@ -17,7 +17,7 @@ export const createProject = (project, history) => async dispatch => {
   }
 };
 export const getAllProject = () => async dispatch => {
-  const res = await axios.get("http://localhost:8080/api/project/all");
+  const res = await axios.get("/api/project/all");
 
   dispatch({
     type: GET_PROJECTS,
@@ -27,7 +27,7 @@ export const getAllProject = () => async dispatch => {
 
 export const getProjectById = (id, history) => async dispatch => {
   try {
-    const res = await axios.get(`http://localhost:8080/api/project/${id}`);
+    const res = await axios.get(`/api/project/${id}`);
     dispatch({
       type: GET_PROJECT,
       payload: res.data
@@ -37,10 +37,12 @@ export const getProjectById = (id, history) => async dispatch => {
   }
 };
 
-export const deleteProject=(id)=>async dispatch=>{
-    await axios.delete(`http://localhost:8080/api/project/${id}`);
+export const deleteProject = id => async dispatch => {
+  if (window.confirm("Are you sure to want to delete it")) {
+    await axios.delete(`/api/project/${id}`);
     dispatch({
-        type:DELETE_PROJECT,
-        payload:id
-    })
-}
+      type: DELETE_PROJECT,
+      payload: id
+    });
+  }
+};
