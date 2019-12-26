5b1c80a2e167a31483e216f4c7d4ffa3d363f9e5
Savinay Sinha <savinaysinha@gmail.com>
2019-12-26 07:03:56 +0530

    fix bug while getting error in update page and still shows when updating another projects

--- ppmtoolfrontend/ppmtool-react-client/src/actions/ProjectActions.js
+++ ppmtoolfrontend/ppmtool-react-client/src/actions/ProjectActions.js
@@ -1,34 +1,38 @@
-import axios from 'axios';
-import {GET_ERRORS, GET_PROJECTS, GET_PROJECT} from './type'
+import axios from "axios";
+import { GET_ERRORS, GET_PROJECTS, GET_PROJECT } from "./type";
 
-
-export const createProject =(project, history)=> async dispatch=>{
-    try {
-        await axios.post("http://localhost:8080/api/project",project);  
-        history.push("/dashboard");
-    } catch (error) {
-        dispatch({
-            type:GET_ERRORS,
-            payload:error.response.data
-        })
-    }
-} 
-export const getAllProject=()=>async dispatch=>{
-    const res=await axios.get("http://localhost:8080/api/project/all");
-   
+export const createProject = (project, history) => async dispatch => {
+  try {
+    await axios.post("http://localhost:8080/api/project", project);
+    history.push("/dashboard");
     dispatch({
-        type:GET_PROJECTS,
-        payload:res.data
-    })
-}
+        type: GET_ERRORS,
+        payload: {}
+      });
+  } catch (error) {
+    dispatch({
+      type: GET_ERRORS,
+      payload: error.response.data
+    });
+  }
+};
+export const getAllProject = () => async dispatch => {
+  const res = await axios.get("http://localhost:8080/api/project/all");
 
-export const getProjectById=(id,history)=>async dispatch=>{
+  dispatch({
+    type: GET_PROJECTS,
+    payload: res.data
+  });
+};
 
+export const getProjectById = (id, history) => async dispatch => {
+  try {
     const res = await axios.get(`http://localhost:8080/api/project/${id}`);
-    console.log(res.data);
-    
     dispatch({
-        type:GET_PROJECT,
-        payload:res.data
-    })
-}
+      type: GET_PROJECT,
+      payload: res.data
+    });
+  } catch (error) {
+    history.push("/dashboard");
+  }
+};
