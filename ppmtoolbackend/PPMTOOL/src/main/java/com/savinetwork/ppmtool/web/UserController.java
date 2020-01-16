9a7adc4c9eda51d4f1d1e2e0a4711db0dde43244
Savinay Sinha <savinaysinha@gmail.com>
2020-01-17 02:28:15 +0530

    folder structure and dependency for security patched

---
+++ ppmtoolbackend/PPMTOOL/src/main/java/com/savinetwork/ppmtool/web/UserController.java
@@ -0,0 +1,13 @@
+package com.savinetwork.ppmtool.web;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/user")
+@CrossOrigin
+public class UserController {
+	
+
+}
